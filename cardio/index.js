424819f4cf97c3f2715af4f683524d593c2560eb
Nathan C <32592701+Wander1ust@users.noreply.github.com>
2018-12-03 17:23:42 +0000

    Add files via upload

---
+++ cardio/index.js
@@ -0,0 +1,70 @@
+// 1: Reverse a string
+//    Return string in reverse
+// ex. reverseString('hello') == 'olleh'
+
+function reverseString(str) {
+    //  Splits a string and reverses it. Finally, it is joined.
+    // return str
+    // .split('')
+    // .reverse()
+    // .join('');
+
+  // ------------------------------
+
+    // let revString = '';
+    // for(let i = str.length -1; i >= 0; i --) {
+    //    revString = revString + str[i];
+    // }
+    // return revString;
+
+// --------------------------------
+
+    // let revString = '';
+    // for(let i = 0; i <= str.length - 1; i ++) {
+    // revString = str[i] + revString;
+    // }
+    // return revString;
+    // }
+
+// -----------------------------------
+
+        // let revString = '';
+        // for(let char of str) {
+        // revString = char + revString;
+        // }
+        // return revString;
+        // }
+
+// ------------------------------------
+
+    // let revString = '';
+    // str.split('').forEach((char) => revString = char + 
+    //     revString);
+
+    // return revString;
+
+//     return str.split('').reduce((revString,char) => char +
+//      revString, '')
+ }
+// ----------------------------------
+
+
+// 2: Validate a palindrome
+//    Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === true, isPalindrome
+// ('hello') == false
+
+function isPalindrome(str) {}
+
+// 3:  Reverse an integer 
+//     Return an integer in reverse
+//     ex. reverseInt(521) === 125
+
+function reverseInt(int) {}
+
+// 4:  Capitalize letters
+//
+
+const output = reverseString('hello');
+
+console.log(output);
